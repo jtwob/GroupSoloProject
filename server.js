d619492bc8267637209173d49fcb48c6d61b0d48
James Totah <jtotah@usfca.edu>
2023-11-30 15:40:00 -0600

    UPDATE: rearranging file structure to support MVC easier

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const routes = require("./routes.js");
+const routes = require("./routes/routes.js");
 const app = express();
 const PORT = 8080;
 
