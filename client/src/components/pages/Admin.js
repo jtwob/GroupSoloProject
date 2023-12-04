a197bb1b9ff30b1e2c61e90e048e167003df95b4
James Totah <jtotah@usfca.edu>
2023-12-04 14:12:09 -0600

    UPDATE: beginning work on client

--- client/src/components/pages/Admin.js
+++ client/src/components/pages/Admin.js
@@ -1,10 +1,8 @@
 import React from "react";
-
 const Admin = () => {
   return (
     <div>
       <p>Admin</p>
-      <a href="http://localhost:8080/api/users/ping">Ping Server</a>
     </div>
   );
 };
