b1537a296b9a392176be99a2ec7d25346579826d
James Totah <jtotah@usfca.edu>
2023-12-10 18:34:00 -0600

    UPDATE: Setting up signup and signed in user context

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,32 @@ import Signup from "./components/pages/Signup";
 import Admin from "./components/pages/Admin";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+
+
 function App() {
+  
+  const UserContext = React.createContext({
+    name: 'Guest',
+    id: null,
+  });
+  
+  const {signedInUser} = this.props;
+
   return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/signup" element={<Signup />}></Route>
-          <Route exact path="/admin" element={<Admin />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <UserContext.Provider value={signedInUser}>
+      <Router>
+        <div>
+          <NavTabs />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+            <Route exact path="/admin" element={<Admin />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,30 +1,28 @@
 import React, {useState} from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+import {UserContext, useContext} from '../../App'
 
 function Signup() {
-  // const history = useHistory();
-  // const [email, setEmail] = useState();
-  // const [password, setPassword] = useState();
-  // const [displayName, setDisplayName] = useState();
-  // const { setUserData } = useContext(UserContext);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [displayName, setDisplayName] = useState();
+  const { setUserData } = useContext(UserContext);
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  //   try {
-  //     const userPost = await axios.post("/api/users", {
-  //       email,
-  //       password,
-  //       displayName,
-  //     });
-      // localStorage.setItem("auth-token", userPost.data._id);
-      // setUserData({ userId: userPost.data._id })
-      // history.push("/");
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    return (<>Signup</>);
+    try {
+      const userPost = await axios.post("/api/users", {
+        email,
+        password,
+        displayName,
+      });
+      setUserData({name: userPost.data.displayName, id: userPost.data._id })
+    } catch (err) {
+      console.log(err);
+    }
   };
-// }
+  
+  return (<>Signup</>);
+}
 export default Signup;
